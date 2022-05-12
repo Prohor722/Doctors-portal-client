f3c57bb2098c8e6ba8cd645c529db706597285f9
Prohor722 <prohor.b722@gmail.com>
2022-05-12 21:29:31 +0600

    navbar basic structure done!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
-import Home from './components/Home/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./Pages/Shared/Navbar";
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Reviews from "./Pages/Reviews/Reviews";
+import Appointment from "./Pages/Appointment/Appointment";
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={ <Home/>}/>
+        <Route path="/about" element={ <About/>}/>
+        <Route path="/reviews" element={ <Reviews/>}/>
+        <Route path="/appointment" element={ <Appointment/>}/>
+        <Route path="/contact" element={ <ContactUs/>}/>
+        <Route path="/login" element={ <Login/>}/>
+      </Routes>
     </div>
   );
 }
