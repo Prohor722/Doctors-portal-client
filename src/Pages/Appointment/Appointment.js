1e77b22f8ab52716ac978dd5975b761a012fea95
Prohor722 <prohor.b722@gmail.com>
2022-05-14 02:00:37 +0600

    appointment page banner added

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
+import AppointmentBanner from './AppointmentBanner';
 
 const Appointment = () => {
     return (
         <div>
-            <h2>this is appointment</h2>
+            <AppointmentBanner/>
+            <Footer/>
         </div>
     );
 };
