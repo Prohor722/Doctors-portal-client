76df3c1cfea7a0d4892c1c867c94e33ae6ba751b
Prohor722 <prohor.b722@gmail.com>
2022-05-14 17:00:41 +0600

    appointment services added

--- src/Pages/Appointment/AppoinmentService.js
+++ src/Pages/Appointment/AppoinmentService.js
@@ -6,16 +6,17 @@ const AppoinmentService = ({ service }) => {
   return (
     <div class="card lg:max-w-lg bg-base-100 shadow-xl">
       <div class="card-body">
-        <h2 class="card-title">{name}</h2>
+        <h2 class="card-title text-secondary">{name}</h2>
         <p>{
             len? 
             <span>
-                
-            </span>: <span></span>
+                {slots[0]} slots available
+            </span>: 
+            <span className="text-red-500">No Slots Available.</span>
             }</p>
             <p>{len}{len>1? ' spaces':' space'} available</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions justify-center">
+          <button disabled={len<1} class="btn btn-secondary text-white">Book Appointment</button>
         </div>
       </div>
     </div>
