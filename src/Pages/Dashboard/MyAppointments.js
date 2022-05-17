dfac13b675139cc6eba3a9290837185dd8b63f2b
Prohor722 <prohor.b722@gmail.com>
2022-05-17 21:48:45 +0600

    my bookings at dashboard showen by table

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
+import Loading from "../Shared/Loading";
 
 const MyAppointments = () => {
-    return (
-        <div>
-            <h2>My Appointments</h2>
-        </div>
-    );
+  const [user, loading] = useAuthState(auth);
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?email=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
+
+//   console.log(appointments);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <div>
+      <h2>My Appointments: {appointments.length}</h2>
+
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Service</th>
+              <th>Time</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appointments.map((appointment,index) => 
+              <tr key={appointment._id}>
+                <th>{index+1}</th>
+                <td>{appointment.patientName}</td>
+                <td>{appointment.treatment}</td>
+                <td>{appointment.slot}</td>
+                <td>{appointment.date}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyAppointments;
