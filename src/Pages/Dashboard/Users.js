b0f448bedf1c49619db2b1241b94eca1355d2ef6
Prohor722 <prohor.b722@gmail.com>
2022-05-18 19:20:07 +0600

    make admin system implemented with make admin button visibility

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,7 +4,7 @@ import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 
 const Users = () => {
-    const {data:users, isLoading} = useQuery('users',()=>fetch('http://localhost:5000/user',{
+    const {data:users, isLoading, refetch} = useQuery('users',()=>fetch('http://localhost:5000/user',{
         method: "GET",
         headers:{
             'authorization': `Bearer ${localStorage.getItem('accessToken')}`
@@ -31,7 +31,7 @@ const Users = () => {
     </thead>
     <tbody>
       {
-          users.map((user,index)=><UserRow key={user._id} user={user} index={index} />)
+          users.map((user,index)=><UserRow key={user._id} user={user} index={index} refetch={refetch} />)
       }
     </tbody>
   </table>
